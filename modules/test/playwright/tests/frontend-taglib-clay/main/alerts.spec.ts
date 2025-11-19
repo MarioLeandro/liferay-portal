024ece557ee143c74a24ede19df431c779f6f5f1
Mario Leandro <mario.leandro@liferay.com>
2025-11-19 13:55:08 -0300

    LPD-71720 Add description level to alerts file to accommodate other types (e.g., Stripe, Feedback, Toast)

--- modules/test/playwright/tests/frontend-taglib-clay/main/alerts.spec.ts
+++ modules/test/playwright/tests/frontend-taglib-clay/main/alerts.spec.ts
@@ -21,73 +21,81 @@ test.beforeEach('Select alerts tab', async ({claySamplePage}) => {
 	await claySamplePage.selectTab(TabName.ALERTS);
 });
 
-test('Verify alert contains all required attributes: icon, type text, and description.', async ({
-	claySamplePage,
-}) => {
-	await test.step('Check if alert-success is present', async () => {
-		await expect(
-			claySamplePage.alert('embedded', 'success').locator.first()
-		).toBeAttached();
+test.describe('Tests for ClayAlert', () => {
+	test('Verify alert contains all required attributes: icon, type text, and description.', async ({
+		claySamplePage,
+	}) => {
+		await test.step('Check if alert-success is present', async () => {
+			await expect(
+				claySamplePage.alert('embedded', 'success').locator.first()
+			).toBeAttached();
+		});
+
+		await test.step('Check if alert-success displays the correct indicator icon', async () => {
+			await expect(
+				claySamplePage
+					.alert('embedded', 'success')
+					.icon('check-circle-full')
+					.first()
+			).toBeVisible();
+		});
+
+		await test.step('Check if alert-success displays the correct lead text', async () => {
+			await expect(
+				claySamplePage
+					.alert('embedded', 'success')
+					.lead('Success')
+					.first()
+			).toBeVisible();
+		});
+
+		await test.step('Check if the alert-success displays description text', async () => {
+			await expect(
+				claySamplePage.alert('embedded', 'success').locator.first()
+			).toHaveText('Success:This is a success message.');
+		});
 	});
 
-	await test.step('Check if alert-success displays the correct indicator icon', async () => {
+	test('Verify the keyword is semi-bold when alert contains status icon and keyword.', async ({
+		claySamplePage,
+	}) => {
+		const successAlert = claySamplePage
+			.alert('embedded', 'success')
+			.locator.first();
+
+		await expect(successAlert).toHaveCSS('font-weight', '400');
+
+		await expect(successAlert.locator('.lead')).toHaveCSS(
+			'font-weight',
+			'600'
+		);
+	});
+
+	test('Verify toast message popup can be closed manually.', async ({
+		claySamplePage,
+	}) => {
+		await claySamplePage.alertSuccessSubmit.trigger.click();
+
+		await clickAndExpectToBeHidden({
+			target: claySamplePage.alertSuccessSubmit.locator,
+			trigger: claySamplePage.alertSuccessSubmit.closeButton,
+		});
+	});
+
+	test('Verify toast message popup will close automatically.', async ({
+		claySamplePage,
+		page,
+	}) => {
+		await claySamplePage.alertDisappearsAfterFiveSeconds.trigger.click();
+
 		await expect(
-			claySamplePage
-				.alert('embedded', 'success')
-				.icon('check-circle-full')
-				.first()
+			claySamplePage.alertDisappearsAfterFiveSeconds.locator
 		).toBeVisible();
-	});
 
-	await test.step('Check if alert-success displays the correct lead text', async () => {
+		await page.waitForTimeout(5000);
+
 		await expect(
-			claySamplePage.alert('embedded', 'success').lead('Success').first()
-		).toBeVisible();
+			claySamplePage.alertDisappearsAfterFiveSeconds.locator
+		).toBeHidden();
 	});
-
-	await test.step('Check if the alert-success displays description text', async () => {
-		await expect(
-			claySamplePage.alert('embedded', 'success').locator.first()
-		).toHaveText('Success:This is a success message.');
-	});
-});
-
-test('Verify the keyword is semi-bold when alert contains status icon and keyword.', async ({
-	claySamplePage,
-}) => {
-	const successAlert = claySamplePage
-		.alert('embedded', 'success')
-		.locator.first();
-
-	await expect(successAlert).toHaveCSS('font-weight', '400');
-
-	await expect(successAlert.locator('.lead')).toHaveCSS('font-weight', '600');
-});
-
-test('Verify toast message popup can be closed manually.', async ({
-	claySamplePage,
-}) => {
-	await claySamplePage.alertSuccessSubmit.trigger.click();
-
-	await clickAndExpectToBeHidden({
-		target: claySamplePage.alertSuccessSubmit.locator,
-		trigger: claySamplePage.alertSuccessSubmit.closeButton,
-	});
-});
-
-test('Verify toast message popup will close automatically.', async ({
-	claySamplePage,
-	page,
-}) => {
-	await claySamplePage.alertDisappearsAfterFiveSeconds.trigger.click();
-
-	await expect(
-		claySamplePage.alertDisappearsAfterFiveSeconds.locator
-	).toBeVisible();
-
-	await page.waitForTimeout(5000);
-
-	await expect(
-		claySamplePage.alertDisappearsAfterFiveSeconds.locator
-	).toBeHidden();
 });
