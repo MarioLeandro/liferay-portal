df0ebafa7e5a9422a9d11b9b2d9f479d5a5fb233
Gabriel Lima <gabriel.lima@liferay.com>
2026-02-13 12:00:52 -0300

    LPD-70107 Use home category children directly inside SideNavigation

--- modules/apps/application-list/application-list-taglib/src/main/java/com/liferay/application/list/taglib/internal/display/context/SideNavigationDisplayContext.java
+++ modules/apps/application-list/application-list-taglib/src/main/java/com/liferay/application/list/taglib/internal/display/context/SideNavigationDisplayContext.java
@@ -111,6 +111,8 @@ public class SideNavigationDisplayContext {
 
 		PanelCategory panelCategory = _getPanelCategory();
 
+		boolean isFirstPanelCategory = true;
+
 		for (PanelCategory childPanelCategory :
 				_panelCategoryHelper.getChildPanelCategories(
 					panelCategory.getKey(), _themeDisplay)) {
@@ -122,12 +124,24 @@ public class SideNavigationDisplayContext {
 				continue;
 			}
 
+			String childPanelCategoryKey = childPanelCategory.getKey();
+
+			if (isFirstPanelCategory) {
+				isFirstPanelCategory = false;
+
+				if (childPanelCategoryKey.endsWith(".home")) {
+					verticalNavItems.addAll(childrenVerticalNavItems);
+
+					continue;
+				}
+			}
+
 			VerticalNavItem verticalNavItem = new VerticalNavItem();
 
 			verticalNavItem.setExpanded(
 				_panelCategoryHelper.containsPortlet(
-					_portletId, childPanelCategory.getKey()));
-			verticalNavItem.setId(childPanelCategory.getKey());
+					_portletId, childPanelCategoryKey));
+			verticalNavItem.setId(childPanelCategoryKey);
 			verticalNavItem.setItems(childrenVerticalNavItems);
 			verticalNavItem.setLabel(
 				childPanelCategory.getLabel(_themeDisplay.getLocale()));
@@ -184,6 +198,8 @@ public class SideNavigationDisplayContext {
 
 		PanelCategory panelCategory = _getPanelCategory();
 
+		boolean isFirstPanelCategory = true;
+
 		for (PanelCategory childPanelCategory :
 				_panelCategoryHelper.getChildPanelCategories(
 					panelCategory.getKey(), _themeDisplay)) {
@@ -195,9 +211,21 @@ public class SideNavigationDisplayContext {
 				continue;
 			}
 
+			String childPanelCategoryKey = childPanelCategory.getKey();
+
+			if (isFirstPanelCategory) {
+				isFirstPanelCategory = false;
+
+				if (childPanelCategoryKey.endsWith(".home")) {
+					propsItems.addAll(childrenPropsItems);
+
+					continue;
+				}
+			}
+
 			propsItems.add(
 				HashMapBuilder.<String, Object>put(
-					"id", childPanelCategory.getKey()
+					"id", childPanelCategoryKey
 				).put(
 					"items", childrenPropsItems
 				).put(
